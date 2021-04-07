b74292cc3e85911a2c457a568f3810b56f9198c8
desainr <eniiseekay@gmail.com>
2021-04-07 17:21:27 -0400

    Initial vue setup using vue-cli and vuetify

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer
+        v-model="drawer"
+        app
+    >
+      <!-- FORM -->
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>NFL Draft History</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <!-- TABLE -->
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+
+export default {
+  data: () => ({ drawer: null }),
+}
+
+</script>
